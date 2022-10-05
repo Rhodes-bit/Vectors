8037e38ec65f9ec63c6233c193145bbba057ffb4
Juan J Rodas <74572519+Rhodes-bit@users.noreply.github.com>
2022-10-04 22:06:53 -0500

    3

--- Assets/Scenes/07 Tweens and Easing/Scripts/Tweens.cs
+++ Assets/Scenes/07 Tweens and Easing/Scripts/Tweens.cs
@@ -5,25 +5,39 @@ using UnityEngine;
 public class Tween : MonoBehaviour
 {
     [SerializeField]  float time;
-    [SerializeField] Transform targetPosition;
+    [SerializeField] Transform targetTransform;
     [SerializeField, Range(0, 1)]  float tparameter = 0;
+    [SerializeField] private Color inicialColor;
+    [SerializeField] private Color finalColor;
     private float currentTime;
-     Vector3 inicialPosition;
+    private Vector3 inicialPosition;
+    private Vector3 targetPosition;
+    private SpriteRenderer spriteRenderer;
+
     
-    void Start()
-    {
-        inicialPosition = transform.position;
-    }
+   private void Start()
+   {
+       spriteRenderer = GetComponent<SpriteRenderer>();
+   }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (currentTime<time)
-        {
-            transform.position = Vector3.Lerp(inicialPosition, targetPosition.position, tparameter);
-        }
-
-        
+        tparameter = currentTime / time;
+       // transform.position = Vector3.Lerp(inicialPosition, targetPosition, tparameter);
+       spriteRenderer.color = Color.Lerp(inicialColor, finalColor, tparameter);
         currentTime += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartTween();
+        }
+    }
+
+    private void StartTween()
+    {
+        tparameter = 0;
+        currentTime = 0;
+        inicialPosition = transform.position;
+        targetPosition = targetTransform.position;
     }
 }
